6305955b0712f2c50c39825a06194642adcda5ac
Bob Roberts <broberts_az@yahoo.com>
2014-03-07 22:06:22 -0700

    added the css and js to precompile

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Simplecrm::Application.config.secret_token = 'b0e61fedbae42ef9f85148978fe88e4c0e5f540c6f66993f7cfefe8468ce609a778a6e3773e43421ff8804949e7321844cfa14cc06eff4df98ae7a15ecd6d2a3'
+Simplecrm::Application.config.secret_key_base = 'bdf106e33e7f16a4ac6cba6d436863a21a3ff1514d461abb4010fa90b41d5cb422fdb48089ab7344251ae11dd6477f19a91f393e19c12e45b119bd408cfd5e2e'
