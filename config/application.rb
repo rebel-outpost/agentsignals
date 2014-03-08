d565824a9ed6e3302daacee220c3a5dad9ad5f2c
Bob Roberts <broberts_az@yahoo.com>
2014-03-07 23:13:46 -0700

    :adjusting the application.rb

--- config/application.rb
+++ config/application.rb
@@ -4,9 +4,9 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module Simplecrm
@@ -61,7 +61,7 @@ module Simplecrm
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = true
-
+    config.assets.initialize_on_precompile = false
 
     config.i18n.enforce_available_locales = true
 
