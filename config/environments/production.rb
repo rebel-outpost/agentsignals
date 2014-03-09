b4a969dbc65cb6ab6485e5c6556e3d37488b0a15
Bob Roberts <broberts_az@yahoo.com>
2014-03-09 14:12:48 -0700

    arg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Simplecrm::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 587,
+    port: 465,
     domain: "gmail.com",
     authentication: :login,
     enable_starttls_auto: true,
