7962a722e7f244c4a433421311b7197bddd43d7c
Bob Roberts <broberts_az@yahoo.com>
2014-03-09 17:17:25 -0700

    hopefully fixing the db migrate

--- db/migrate/20140310000639_add_field_to_opportunities.rb
+++ db/migrate/20140310000639_add_field_to_opportunities.rb
@@ -1,6 +1,5 @@
 class AddFieldToOpportunities < ActiveRecord::Migration
   def change
-    remove_column :opportunities, :opportunity_typ
     add_column :opportunities, :opportunity_type, :string
     remove_column :opportunities, :type
   end
