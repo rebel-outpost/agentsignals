5c43667453cef8de8f3a4d57e33452462fea04bd
Bob Roberts <broberts_az@yahoo.com>
2014-04-04 15:41:03 -0700

    trying to get the damned schema uploaded

--- db/schema.rb
+++ db/schema.rb
@@ -8,7 +8,7 @@
 # system, you should be using db:schema:load, not running all the migrations
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
-#
+# omg omg
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20140404220832) do
