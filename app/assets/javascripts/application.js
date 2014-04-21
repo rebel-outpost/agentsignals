9d078650f2aae5fdff769157f0384817d27f0c0a
Bob Roberts <broberts_az@yahoo.com>
2014-04-21 16:27:34 -0700

    trying to get the new template to work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,13 +8,3 @@ $(function() {
   $(".chosen-select").select2();
   $(".chosen-deselect").select2({ allowClear: true });
 });
-
-$(document).ready(function() {
-    $('#banner-fade').bjqs({
-      height        : 320,
-      width         : 620,
-      responsive    : true,
-      randomstart   : true,
-      showcontrols  : false
-    });
-});
