7e829fcf5c32fa9506a049571628ee7cdbc3a3e3
Bob Roberts <broberts_az@yahoo.com>
2014-04-25 13:23:16 -0700

    fixed the form inputs, added select2 and datatables, edited the index and forms

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,11 +4,14 @@
 //= require jquery.ui.widget
 //= require jquery.ui.mouse
 //= require jquery.ui.draggable
+//= require dataTables/jquery.dataTables
+//= require dataTables/jquery.dataTables.bootstrap3
 //= require bootstrap
+//= require select2
 //= require_tree .
 
 
-// $(function() {
-//   $(".chosen-select").select2();
-//   $(".chosen-deselect").select2({ allowClear: true });
-// });
+$(function() {
+  $(".chosen-select").select2();
+  $(".chosen-deselect").select2({ allowClear: true });
+});
